7e7d76794d4e68790e62757957263661b9000bf8
Dan Allan <dallan@bnl.gov>
2020-02-21 16:46:54 -0500

    Get multiple inheritance right.

--- accumulate_fill_publish.py
+++ accumulate_fill_publish.py
@@ -12,8 +12,9 @@ print = functools.partial(print, file=sys.stderr)
 
 
 class EmittingDocumentRouter(DocumentRouter):
-    def __init__(self, callback):
+    def __init__(self, *args, callback, **kwargs):
         self._callback = callback
+        super().__init__(*args, **kwargs)
 
     def emit(self, name, doc):
         self._callback(name, doc)
@@ -33,7 +34,7 @@ class Accumulator(EmittingDocumentRouter):
             self.emit(*self._cache.popleft())
 
 
-class EmittingFiller(Filler, EmittingDocumentRouter):
+class EmittingFiller(EmittingDocumentRouter, Filler):
     def __call__(self, name, doc, validate=False):
         name, doc = super().__call__(name, doc, validate)
         self.emit(name, doc)
